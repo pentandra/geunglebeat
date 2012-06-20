43b299a7c557419833d7d4c717464b11f9d1656b
Chris Chapman <chris@pentandra.com>
2012-06-20 10:53:16 -0600

    Adding sharrre social widget

--- content/js/script.js
+++ content/js/script.js
@@ -3,6 +3,39 @@
 */
 
 
+$(document).ready(function() {
+
+  if ($.fn.sharrre) {
+    $('#share').sharrre({
+
+      share: {
+        googlePlus: true,
+        facebook: true,
+        twitter: true
+      },
+
+      buttons: {
+        googlePlus: { size: 'tall' },
+        facebook: { layout: 'box_count' },
+        twitter: { count: 'vertical', via: 'PentandraInc' }
+      },
+
+      urlCurl: '',
+
+      enableHover: false,
+      enableCounter: false,
+      enableTracking: true
+      
+    });
+
+  }
+
+  $('a[rel="external"]').click(function() {
+    window.open( $(this).attr('href') );
+    return false;
+  });
+  
+});
 
 
 
